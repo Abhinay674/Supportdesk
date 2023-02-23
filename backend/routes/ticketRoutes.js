0a4e50c6d60d611abd2619adbe252f5b978e165c
Abhinay <makumar674@gmail.com>
2023-02-23 19:46:24 +0530

    'commit'

---
+++ backend/routes/ticketRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {getTickets,createTicket,updateTicket,deleteTicket,getTicket} = require('../controllers/ticketController');
+const router = express.Router();
+const {protect} = require('../middleware/authMiddleware');
+
+// Re-route into note router
+const noteRouter = require('./noteRoutes');
+router.use('/:ticketId/notes',noteRouter)
+
+router.route('/').get(protect,getTickets).post(protect,createTicket);
+router.route('/:id').get(protect,getTicket).put(protect,updateTicket).delete(protect,deleteTicket);
+
+
+module.exports = router;
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const {registerUser,loginUser,getMe} = require('../controllers/userController');
+const router = express.Router();
+const {protect} = require('../middleware/authMiddleware');
+
+
+router.route('/').post(registerUser);
+router.route('/login').post(loginUser);
+router.route('/me').get(protect,getMe);
+
+module.exports = router;
